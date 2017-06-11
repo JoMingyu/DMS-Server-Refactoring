2a80f965a7e116857b7fd7ed4996c5bb766a3a33
JoMingyu <city7310@naver.com>
2017-06-11 22:36:22 +0900

    관리자 로그인 구현

--- src/main/java/com/dms/restful/account/Signin_Admin.java
+++ src/main/java/com/dms/restful/account/Signin_Admin.java
@@ -1,14 +1,88 @@
 package com.dms.restful.account;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
 import com.dms.support.routing.API;
 import com.dms.support.routing.REST;
 import com.dms.support.routing.Route;
+import com.dms.support.util.AES256;
+import com.dms.support.util.MySQL;
+import com.dms.support.util.SHA256;
+import com.dms.support.util.SessionUtil;
 
+import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
 
 @API(functionCategory = "계정", summary = "로그인 - 관리자")
-@REST(requestBody = "", successCode = 0, failureCode = 0, etc = "")
+@REST(requestBody = "id : String, password : String, keep_login : boolean", successCode = 201, failureCode = 204, etc = "쿠키나 세션 존재")
 @Route(uri = "/account/signin/admin", method = HttpMethod.POST)
-public class Signin_Admin {
-
+public class Signin_Admin implements Handler<RoutingContext> {
+	@Override
+	public void handle(RoutingContext ctx) {
+		String id = AES256.encrypt(ctx.request().getFormAttribute("id"));
+		String password = SHA256.encrypt(ctx.request().getFormAttribute("password"));
+		boolean keepLogin = Boolean.parseBoolean(ctx.request().getFormAttribute("keep_login"));
+		
+		ResultSet rs = MySQL.executeQuery("SELECT COUNT(*) FROM admin_account WHERE id=? AND password=?", id, password);
+        try {
+        	if(rs.next()) {
+        		String sessionId = getSessionFromId(id);
+        		
+        		if(sessionId == null) {
+        			sessionId = createSessionId();
+        		}
+        		
+        		if(keepLogin) {
+        			SessionUtil.createCookie(ctx, "AdminSession", sessionId);
+        		} else {
+        			SessionUtil.createSession(ctx, "AdminSession", sessionId);
+        		}
+        		
+        		MySQL.executeUpdate("UPDATE admin_account SET session_id=? WHERE id=?", SHA256.encrypt(sessionId), id);
+				
+				ctx.response().setStatusCode(201).end();
+				ctx.response().close();
+        	} else {
+        		ctx.response().setStatusCode(204).end();
+        		ctx.response().close();
+        	}
+        } catch(SQLException e) {
+        	e.printStackTrace();
+        }
+	}
+	
+	private String getSessionFromId(String id) {
+		ResultSet rs = MySQL.executeQuery("SELECT * FROM admin_account WHERE id=?", id);
+		try {
+			if(rs.next()) {
+				if(rs.getString("session_id") != null) {
+					return rs.getString("session_id");
+				} else {
+					return null;
+				}
+			} else {
+				return null;
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private String createSessionId() {
+		while(true) {
+			String uuid = SHA256.encrypt(UUID.randomUUID().toString());
+			ResultSet rs = MySQL.executeQuery("SELECT * FROM admin_account WHERE session_id=?", uuid);
+			try {
+				if(!rs.next()) {
+					return uuid;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- src/main/java/com/dms/support/account/AdminManager.java
+++ src/main/java/com/dms/support/account/AdminManager.java
@@ -2,84 +2,14 @@ package com.dms.support.account;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.UUID;
 
-import com.dms.support.util.AES256;
 import com.dms.support.util.MySQL;
-import com.dms.support.util.SHA256;
 import com.dms.support.util.SessionUtil;
 
 import io.vertx.ext.web.RoutingContext;
 
 public class AdminManager {
 	private static ResultSet rs;
-	
-	private String getSessionFromId(String id) {
-		rs = MySQL.executeQuery("SELECT * FROM admin_account WHERE id=?", id);
-		try {
-			if(rs.next()) {
-				if(rs.getString("session_id") != null) {
-					return rs.getString("session_id");
-				} else {
-					return null;
-				}
-			} else {
-				return null;
-			}
-		} catch(SQLException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	private String createSessionId() {
-		String uuid;
-		
-		while(true) {
-			uuid = SHA256.encrypt(UUID.randomUUID().toString());
-			rs = MySQL.executeQuery("SELECT * FROM admin_account WHERE session_id=?", uuid);
-			try {
-				if(!rs.next()) {
-					return uuid;
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public void registerSessionId(RoutingContext ctx, boolean keepLogin, String id) {
-		id = AES256.encrypt(id);
-		
-		String sessionId = getSessionFromId(id);
-		
-		if(sessionId == null) {
-			sessionId = createSessionId();
-		}
-		
-		if(keepLogin) {
-			SessionUtil.createCookie(ctx, "AdminSession", sessionId);
-		} else {
-			SessionUtil.createSession(ctx, "AdminSession", sessionId);
-		}
-	}
-	
-	public boolean signIn(String id, String password) {
-		id = AES256.encrypt(id);
-		password = SHA256.encrypt(password);
-		
-		rs = MySQL.executeQuery("SELECT COUNT(*) FROM admin_account WHERE id=? AND password=?", id, password);
-        try {
-        	if(rs.next()) {
-        		return true;
-        	} else {
-        		return false;
-        	}
-        } catch(SQLException e) {
-        	e.printStackTrace();
-        	return false;
-        }
-	}
 	
 	public static String getEncryptedIdFromSession(RoutingContext ctx) {
 		String sessionId = SessionUtil.getSessionId(ctx, "AdminSession");
